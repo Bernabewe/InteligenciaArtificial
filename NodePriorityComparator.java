c0c0fbab2b0a935b8f57cd407ac2690f3dcbfea0
Bernardo Bejarano <bernybf05@gmail.com>
2026-02-23 08:30:00 -0700

    NodePriorityComparator

---
+++ NodePriorityComparator.java
@@ -0,0 +1,16 @@
+
+import java.util.Comparator;
+
+public class NodePriorityComparator implements Comparator<Nodo> {
+ 
+    @Override
+    public int compare(Nodo x, Nodo y) {
+        if (x.getTotalCost() < y.getTotalCost()) {
+            return -1;
+        }
+        if (x.getTotalCost() > y.getTotalCost()) {
+            return 1;
+        }
+        return 0;
+    }
+}
